522f200bf0e61f2d1bd1b65437edbde599900d7a
tyler ferre <ferre.tyler1@gmail.com>
2022-09-21 15:11:55 -0600

    first commit

---
+++ event-listener-practice/app.js
@@ -0,0 +1,44 @@
+// var box = document.getElementById("box")
+// document.getElementById("box")
+// .addEventListener("mouseover", function(){
+//     var blue = document.querySelector("#box").classList.toggle("blue")
+// }
+// )
+
+var box = document.getElementById("box")
+document.getElementById("box")
+.addEventListener("mouseover", function(){
+box.style.backgroundColor = "blue"
+}
+)
+var box = document.getElementById("box")
+document.getElementById("box")
+.addEventListener("mousedown", function(){
+box.style.backgroundColor = "red"
+}
+)
+var box = document.getElementById("box")
+document.getElementById("box")
+.addEventListener("mouseleave", function(){
+box.style.backgroundColor = "white"
+}
+)
+var box = document.getElementById("box")
+document.getElementById("box")
+.addEventListener("dblclick", function(){
+box.style.backgroundColor = "green"
+}
+)
+var box = document.getElementById("box")
+document.getElementById("box")
+.addEventListener("mouseup",  function(){
+box.style.backgroundColor = "yellow"
+}
+)
+var box = document.getElementById("box")
+document.getElementById("box")
+.addEventListener("wheel", function(){
+box.style.backgroundColor = "orange"
+}
+)
+
