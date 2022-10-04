6393b706524f0e7da2a70f193c88dc508ea9c92d
tyler ferre <ferre.tyler1@gmail.com>
2022-10-03 21:11:05 -0600

    first commit

--- event-listener-practice/app.js
+++ event-listener-practice/app.js
@@ -35,10 +35,59 @@ document.getElementById("box")
 box.style.backgroundColor = "yellow"
 }
 )
+
+
 var box = document.getElementById("box")
-document.getElementById("box")
-.addEventListener("wheel", function(){
-box.style.backgroundColor = "orange"
+window.addEventListener("wheel", function(){
+    box.style.backgroundColor = "orange" 
 }
 )
 
+window.addEventListener("keypress", function (o) {
+    if (o.key === "o") {
+        box.style.backgroundColor = "orange"
+    }
+}
+)
+window.addEventListener("keypress", function (r) {
+    if (r.key === "r") {
+        box.style.backgroundColor = "red"
+    }
+}
+)
+window.addEventListener("keypress", function(w){
+    if(w.key === "w") {
+        box.style.backgroundColor = "white"
+    }
+}
+)
+window.addEventListener("keypress", function(g){
+    if(g.key === "g") {
+        box.style.backgroundColor = "green"
+    }
+}
+)
+window.addEventListener("keypress", function(p){
+    if(p.key === "p") {
+        box.style.backgroundColor = "rgb(194, 153, 255)"
+    }
+}
+)
+window.addEventListener("keypress", function(b){
+    if(b.key === "b") {
+        box.style.backgroundColor = "blue"
+    }
+})
+window.addEventListener("keypress", function(y){
+    if(y.key === "y") {
+        box.style.backgroundColor = "yellow"
+    }
+})
+var enter = document.getElementById("enter")
+.addEventListener("click", function(){
+    var colorPicked = document.getElementById("colorInput").value;
+    var changedColor = box.style.backgroundColor = colorPicked
+    var boxInput = document.getElementById("box")
+    // boxInput.append(changedColor)
+}
+)
