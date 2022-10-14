87d0196f380602d6716286f6769627c23f5d3296
tyler ferre <ferre.tyler1@gmail.com>
2022-10-14 16:23:11 -0600

    first commit for employees-records

---
+++ forception/app.js
@@ -0,0 +1,15 @@
+// people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
+// alphabet = "abcdefghijklmnopqrstuvwxyz"
+//     var alph = alphabet.split("")
+//         for(var i = 0; i < people.length; i++){
+//             for(var a = 0; a < people[i].length; a++){
+//             // console.log(people[i] + ": " + alph)
+//            var final =  people[i] + ": ".concat(alph)
+//            console.log(final)
+//             }
+//         }
+
+function forception(people, alphabet){
+    people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+}
